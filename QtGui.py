7889394d20d3c2a5ec9f5abc13178bdfc3c069f2
EwaKomkowska <37376908+EwaKomkowska@users.noreply.github.com>
2020-01-20 20:30:49 +0100

    without RTC

--- QtGui.py
+++ QtGui.py
@@ -17,15 +17,10 @@ import sqlite3
 
 def baza():
     cur.execute("create table if not exists Cards(id INT, name TEXT, data TEXT)")
-    cur2.execute("create table if not exists Logs(id INT)")
 
     try:
         cur.execute("insert into Cards values(1, 'nazwa', 'data')")
-        cur.execute("insert into Cards values(2, 'nazwa2', 'data2')")
-        cur.execute("insert into Cards values(3, 'nazwa3', 'data3')")
-        #cur.execute("delete from Cards where id = 1")
-        cur2.execute("insert into Logs values(1)")
-        #cur2.execute("delete from Logs where id = 1")
+        cur.execute("delete from Cards where id = 1")
 
     except sqlite3.Error as e:
         print("Error {}:".format(e.args[0]))
@@ -45,6 +40,7 @@ def openDoor():
     if number > 0:
         print("Welcome to the sky, Mr/Mrs")
         return True
+    return False
 
 
 def writeIntoDatabase():
@@ -86,12 +82,12 @@ class MainWindow(QWidget):
         self.tableList.setRowCount(0)
 
         self.label = QLabel(self)
-        self.label.setText("Instrukcja obsługi:\n1 - wyświetl całą bazę\n2 - wyczyść bazę")
+        self.label.setText("Instruction: \n1 - Write\n2 - Open the door\n3 - exit\n4 - drop\n5 - select all")
         self.label.setStyleSheet(" font-size: 15px; font-family: Courier New;")  # qproperty-alignment: AlignJustify;
         self.label.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
 
         self.info = QLabel(self)
-        self.info.setText("Tu jest label do wyświetlania wyniku procedury")
+        self.info.setText("Nothing")
         self.info.setStyleSheet(" font-size: 15px; font-family: Courier New; qproperty-alignment: AlignRight")
         self.info.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
 
@@ -102,23 +98,9 @@ class MainWindow(QWidget):
         #podłączenie do bazy
         baza()
 
-        '''self.pybutton = QPushButton('OK', self)
-        self.pybutton.clicked.connect(self.clickMethod)
-        self.pybutton.move(80, 60)'''
-
-        self.timer = QTimer(self)
-        self.timer.setInterval(1000)
-        self.timer.timeout.connect(self.recurring_timer)
-        self.timer.start()
-
-    def recurring_timer(self):
-        self.counter += 1
-        self.label.setText("Counter: %d" % self.counter)
-
     def init_TableList(self):
         cur.execute('''SELECT * FROM Cards ''')
         rows = cur.fetchall()
-        print("Len rows: ", len(rows))
 
         self.tableList.setEnabled(False)            #wygląda śrendnio, ale działa
         self.tableList.setRowCount(len(rows))
@@ -136,28 +118,54 @@ class MainWindow(QWidget):
             self.tableList.setItem(counter, 1, QTableWidgetItem(row[1]))
             self.tableList.setItem(counter, 2, QTableWidgetItem(row[2]))
             counter += 1
-            print(counter)
-
-        #cardsRows = [[1, "Id 0", "data 0"], [2, "Id 1", "data 1"]]
 
     def keyPressEvent(self, event):
-        # dodać obsługę bazy
         try:
             if event.key() == Qt.Key_1:
                 writeIntoDatabase()
+                self.info.setText("New data in database")
+                self.timer = QTimer(self)
+                self.timer.setInterval(10000)
+                self.timer.timeout.connect(self.changeLabel)
+                self.timer.start()
                 print("Zapisanie do bazy")
+
             elif event.key() == Qt.Key_2:
-                openDoor()
+                #openDoor()
+                self.info.setText("The door is open")
+                self.timer = QTimer(self)
+                self.timer.setInterval(10000)
+                self.timer.timeout.connect(self.changeLabel)
+                self.timer.start()
                 print("A teraz drzwi")
+
             elif event.key() == Qt.Key_3:
-                exit(0)
+                '''GPIO.cleanup()'''
+                self.info.setText("Goodbye")
+                if con:
+                    con.close()
                 print("Wychodzimy")
+                exit(0)
+
             elif event.key() == Qt.Key_4:
                 cur.execute("drop table Cards")
+                self.info.setText("Nothing in database")
+                self.timer = QTimer(self)
+                self.timer.setInterval(10000)
+                self.timer.timeout.connect(self.changeLabel)
+                self.timer.start()
                 print("Czyszczenie bazy")
+                baza()      #zeby po wyczyszczeniu istniala dalej tablica
+
             elif event.key() == Qt.Key_5:
                 self.init_TableList()
+                self.info.setText("You see all data")
+                self.timer = QTimer(self)
+                self.timer.setInterval(10000)
+                self.timer.timeout.connect(self.changeLabel)
+                self.timer.start()
                 print("wyswietlam wszystkie")
+
             else:
                 print('Wrong output')
 
@@ -168,53 +176,19 @@ class MainWindow(QWidget):
             print("Error {}:".format(e.args[0]))
             exit(0)
 
-        '''finally:
-            GPIO.cleanup()
-            if con:
-                con.close()'''
-
-        # odczyt karty - tutaj ma byc po prostu dodanie do bazy logów?
-        # cardID, text = 1, "text" '''= reader.read()'''
-        # wyświetlenie nowego przedmiotu w tabeli - trzeba stworzyć dodakową bazę na logi
-        #
-
-    def clickMethod(self):
-        time.sleep(5)
-
     def initGrid(self):
         self.init_TableList()
 
-        cur.execute("select * from Cards")
-        rowDatabase = cur.fetchall()  # 2
-        print(rowDatabase)
-        print("Len: ", len(rowDatabase))
-
-        logsDatabaseRow = cur2.fetchall()   #5
-        logsRows = []        #= ["Log1", "Log2", "Log3", "Log4", "Log5"]
-        for elem in logsDatabaseRow:
-            logsRows.append(elem)
-
-        self.logs = QTableWidget(self)
-        #self.logs.setEditTriggers(QAbstractItemView.SelectedClicked)         #ż☺eby nie móc edytować tabeli
-        self.logs.setEnabled(False)
-        self.logs.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.logs.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)  # rozciąganie kolumn
-        self.logs.setRowCount(len(logsDatabaseRow))
-
-        self.logs.setColumnCount(1)
-        self.logs.setHorizontalHeaderLabels(["Logs(CardID)"])
-
-        # tutaj odczytujemy dane z bazy
-        for i in range(len(logsDatabaseRow)):
-            self.logs.setItem(i, 0, QTableWidgetItem(logsRows[i]))
+    def changeLabel(self):
+        self.info.setText("Nothing")
 
     def setGrid(self):
-        self.layout = QGridLayout(self)
-        self.layout.addWidget(self.label, 0, 0)
-        self.layout.addWidget(self.info, 0, 1)
-        self.layout.addWidget(self.tableList, 1, 0)
-        self.layout.addWidget(self.logs, 1, 1)
-
+        self.layout = QVBoxLayout(self)
+        self.layout2 = QHBoxLayout(self)
+        self.layout2.addWidget(self.label)
+        self.layout2.addWidget(self.info)
+        self.layout.addLayout(self.layout2)
+        self.layout.addWidget(self.tableList)
         self.setLayout(self.layout)
 
 
@@ -222,9 +196,7 @@ if __name__ == "__main__":
 
     app = QApplication(sys.argv)
     con = sqlite3.connect('cardsData.db')
-    con2 = sqlite3.connect('logsData.db')
     cur = con.cursor()
-    cur2 = con2.cursor()
 
     while 1:
         ex = MainWindow()
